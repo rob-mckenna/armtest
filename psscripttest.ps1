c839540df89e3eaa0c3a619fb83e4164e832d38a
Rob McKenna <14824673+rob-mckenna@users.noreply.github.com>
2021-02-23 23:04:30 -0500

    test

--- psscripttest.ps1
+++ psscripttest.ps1
@@ -1,11 +1,11 @@
 param([string]$File)
-#$FileList = $File  -replace '(\\[|\\])' -split ' ' | Foreach-Object {$_.trim()}
+$FileList = $File  -replace '(\\[|\\])' -split ' ' | Foreach-Object {$_.trim()}
 $StorageAccount = Get-AzStorageAccount -ResourceGroupName $env:ResourceGroupName -Name $env:StorageAccountName -Verbose
 $Count = 0
 $DeploymentScriptOutputs = @{}
 foreach ($FileName in $FileList) {
-    #Write-Host \"Copying $FileName to $env:StorageContainerName in $env:StorageAccountName.\"
-    #Invoke-RestMethod -Uri \"https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/mslearn-arm-deploymentscripts-sample/$FileName\" -outfile $FileName
+    Write-Host \"Copying $FileName to $env:StorageContainerName in $env:StorageAccountName.\"
+    Invoke-RestMethod -Uri \"https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/mslearn-arm-deploymentscripts-sample/$FileName\" -outfile $FileName
     $Blob = Set-AzStorageBlobContent -File $FileName -Container $env:StorageContainerName -blob $FileName -context $StorageAccount.Context
     $DeploymentScriptOutputs[$FileName] = @{}
     $DeploymentScriptOutputs[$FileName]['Uri'] = $Blob.ICloudBlob.Uri
