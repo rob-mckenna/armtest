787b967fed04c375cb835f38e9f62033337c031a
Rob McKenna <14824673+rob-mckenna@users.noreply.github.com>
2021-02-23 23:48:24 -0500

    test

--- psscripttest.ps1
+++ psscripttest.ps1
@@ -5,10 +5,11 @@ $Count = 0
 $DeploymentScriptOutputs = @{}
 foreach ($FileName in $FileList) {
     Write-Host "Copying $FileName to $env:StorageContainerName in $env:StorageAccountName."
-    Invoke-RestMethod -Uri "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/mslearn-arm-deploymentscripts-sample/$FileName" -outfile $FileName
-    $Blob = Set-AzStorageBlobContent -File $FileName -Container $env:StorageContainerName -blob $FileName -context $StorageAccount.Context
-    $DeploymentScriptOutputs[$FileName] = @{}
-    $DeploymentScriptOutputs[$FileName]['Uri'] = $Blob.ICloudBlob.Uri
-    $DeploymentScriptOutputs[$FileName]['StorageUri'] = $Blob.ICloudBlob.StorageUri
+    # Invoke-RestMethod -Uri "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/mslearn-arm-deploymentscripts-sample/$FileName" -outfile $FileName
+    # $Blob = Set-AzStorageBlobContent -File $FileName -Container $env:StorageContainerName -blob $FileName -context $StorageAccount.Context
+    # $DeploymentScriptOutputs[$FileName] = @{}
+    # $DeploymentScriptOutputs[$FileName]['Uri'] = $Blob.ICloudBlob.Uri
+    # $DeploymentScriptOutputs[$FileName]['StorageUri'] = $Blob.ICloudBlob.StorageUri
     $Count++
 }
+Write-Host "Finished copying $Count files."
