3cf739dfc9f554226438cb528d8c9c849c7c3129
Rob McKenna <14824673+rob-mckenna@users.noreply.github.com>
2021-02-23 23:27:27 -0500

    test

--- psscripttest.ps1
+++ psscripttest.ps1
@@ -4,8 +4,8 @@ $StorageAccount = Get-AzStorageAccount -ResourceGroupName $env:ResourceGroupName
 $Count = 0
 $DeploymentScriptOutputs = @{}
 foreach ($FileName in $FileList) {
-    Write-Host \"Copying $FileName to $env:StorageContainerName in $env:StorageAccountName.\"
-    Invoke-RestMethod -Uri \"https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/mslearn-arm-deploymentscripts-sample/$FileName\" -outfile $FileName
+    Write-Host "Copying $FileName to $env:StorageContainerName in $env:StorageAccountName."
+    Invoke-RestMethod -Uri "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/mslearn-arm-deploymentscripts-sample/$FileName" -outfile $FileName
     $Blob = Set-AzStorageBlobContent -File $FileName -Container $env:StorageContainerName -blob $FileName -context $StorageAccount.Context
     $DeploymentScriptOutputs[$FileName] = @{}
     $DeploymentScriptOutputs[$FileName]['Uri'] = $Blob.ICloudBlob.Uri
